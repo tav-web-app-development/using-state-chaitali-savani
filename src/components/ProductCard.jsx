a697cd32606a50eb50c135d3d21f7fd2b43c8f22
chaitali <savanichaitali@gmail.com>
2024-03-09 22:09:52 -0500

    Using State Assignment completed

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,15 @@
-export default function ProductCard({ product }) {
-  let currentImageIndex = 0;
-  let itemsInCart = 0;
+import { useState } from "react";
 
-  const handleAddToCartClick = () => {
-    itemsInCart++;
-    alert(`you added ${itemsInCart}`);
-  };
+let itemsInCart = 0;
+const handleAddToCartClick = () => {
+  itemsInCart++;
+  alert(`you added ${itemsInCart}`);
+};
+
+export default function ProductCard({ product }) {
+  const [currentImageIndex, setcurrentImageIndex]  = useState(0);
+  const [isClick, setClick] = useState(true);
+  
   return (
     <>
       <div id="image-carousel">
@@ -13,16 +17,19 @@ export default function ProductCard({ product }) {
           src={product.imageUrls[currentImageIndex] + " " + product.name}
           alt={product.name}
         />
-        <button>Next</button>
-        <button>Previous</button>
+      
+        <button disabled={currentImageIndex == product.imageUrls.length-1 ? true : false} onClick={() => setcurrentImageIndex(currentImageIndex + 1)}>Next</button>
+        <button disabled={currentImageIndex ? false : true} onClick={() => setcurrentImageIndex(currentImageIndex - 1)}>Previous</button>
       </div>
 
       <h3>{product.name}</h3>
-      <p>{product.description}</p>
-      <button>Show Description</button>
+      <p>{isClick ?"" :product.description}</p>
+      <button onClick={()=> setClick(!isClick)}> {isClick ? "Show Description":"Hide Description"} </button>
+      
+      
       <div className="price">${product.price}</div>
 
-      <button onClick={handleAddToCartClick}>Add to Cart</button>
+      <button disabled={product.isInStock ? false : true} onClick={handleAddToCartClick}>Add to Cart</button>
 
       {!product.isInStock && "The product is out of stock"}
     </>
